d19179c6b4c94f16f5effc032f90de8249e88e28
Deyan Nikolov <didonikolovbg@gmail.com>
2023-08-06 22:41:26 +0300

    Social Login Urls
    Co-Authored-By: Samuil Velikov <94323419+SvPhxc@users.noreply.github.com>

--- website/adventora/urls.py
+++ website/adventora/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("", include("home.urls")),
     path("authentication/", include("authentication.urls")),
     path("api/", include("api.urls")),
+    path('social/', include('social_django.urls', namespace='social')),
 ]
