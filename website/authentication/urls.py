9bf5e032f4b4a10a62b06915fd2a1ff1fff65529
Deyan Nikolov <didonikolovbg@gmail.com>
2023-08-06 22:41:51 +0300

    User Profile Control
    Co-Authored-By: Samuil Velikov <94323419+SvPhxc@users.noreply.github.com>

--- website/authentication/urls.py
+++ website/authentication/urls.py
@@ -5,4 +5,11 @@ urlpatterns = [
     path('sign-up', views.sign_up, name='sign-up'),
     path('sign-in', views.sign_in, name='sign-in'),
     path('logout', views.log_out, name='logout'),
+    path('choose-role', views.choose_role, name='choose-role'),
+    path('complete-name', views.complete_name, name='complete-name'),
+    path('complete-email', views.complete_email, name='complete-email'),
+    path('complete-username', views.complete_username, name='complete-username'),
+    path('edit-profile', views.edit_profile, name='edit-profile'),
+    path('change-password', views.change_password, name='change-password'),
+    path('delete-account', views.delete_account, name='delete-account'),
 ]
--- website/authentication/views.py
+++ website/authentication/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth import login, authenticate, logout, update_session_auth_hash
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from .forms import RegisterForm, LoginForm
+from .forms import RegisterForm, LoginForm, RoleChoiceForm, CompleteNameForm, CompleteEmailForm, CompleteUserNameForm, EditProfileForm, ChangePasswordForm, SetPasswordFromSocialLogin, DeleteAccountForm
 from django.contrib import messages
+
+from .models import CustomUser
 
 
 def sign_up(request):
@@ -118,3 +121,222 @@ def log_out(request):
     logout(request)
     messages.success(request, 'Успешно излизане.')
     return redirect('home')
+
+
+@login_required
+def choose_role(request):
+
+    if request.method == 'POST':
+        form = RoleChoiceForm(request.POST)
+        if form.is_valid():
+            role_choices = ["user", "hotel_manager", "employee"]
+            role = form.cleaned_data.get('role')
+            if role not in role_choices:
+                messages.error(request, 'Невалидна роля.')
+                return redirect('choose-role')
+            request.user.role = role
+            request.user.confirmedrole = True
+            request.user.save()
+            messages.success(request, 'Успешно избрана роля.')
+            return redirect('home')
+
+    form = RoleChoiceForm()
+    return render(request, 'auth/choose_role.html', {'form': form})
+
+
+@login_required
+def complete_name(request):
+    if request.method == 'POST':
+        form = CompleteNameForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data.get('first_name')
+            last_name = form.cleaned_data.get('last_name')
+
+            request.user.first_name = first_name
+            request.user.last_name = last_name
+            request.user.confirmedname = True
+            request.user.save()
+            messages.success(request, 'Успешно попълнено име.')
+            return redirect('home')
+
+    form = CompleteNameForm(initial={'first_name': request.user.first_name, 'last_name': request.user.last_name})
+    first_name = request.user.first_name
+    last_name = request.user.last_name
+    context = {
+        'form': form,
+        'first_name': first_name,
+        'last_name': last_name
+    }
+    return render(request, 'auth/complete_name.html', context)
+
+
+@login_required
+def complete_email(request):
+    if request.method == 'POST':
+        form = CompleteEmailForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+
+            existing_user = CustomUser.objects.filter(email=email).first()
+            if existing_user is not None:
+                if existing_user != request.user:
+                    messages.error(request, 'Потребител с този имейл вече съществува.')
+                    return redirect('complete-email')
+
+            request.user.email = email
+            request.user.confirmedemail = True
+            request.user.save()
+            messages.success(request, 'Успешно попълнен имейл.')
+            return redirect('home')
+
+    form = CompleteEmailForm(initial={'email': request.user.email})
+    email = request.user.email
+    context = {
+        'form': form,
+        'email': email,
+    }
+    return render(request, 'auth/complete_email.html', context)
+
+
+@login_required
+def complete_username(request):
+    if request.method == 'POST':
+        form = CompleteUserNameForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+
+            existing_user = CustomUser.objects.filter(username=username).first()
+            if existing_user is not None:
+                if existing_user != request.user:
+                    messages.error(request, 'Потребител с това потребителско име вече съществува.')
+                    return redirect('complete-username')
+
+            request.user.username = username
+            request.user.confirmedusername = True
+            request.user.save()
+            messages.success(request, 'Успешно попълнено потребителско име.')
+            return redirect('home')
+
+    form = CompleteUserNameForm(initial={'username': request.user.username})
+    username = request.user.username
+    context = {
+        'form': form,
+        'username': username,
+    }
+    return render(request, 'auth/complete_username.html', context)
+
+
+def edit_profile(request):
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data.get('first_name')
+            last_name = form.cleaned_data.get('last_name')
+
+            email = form.cleaned_data.get('email')
+            existing_user = CustomUser.objects.filter(email=email).first()
+            if existing_user is not None and existing_user != request.user:
+                messages.error(request, 'Потребител с този имейл вече съществува.')
+                return redirect('edit-profile')
+
+            username = form.cleaned_data.get('username')
+            existing_user = CustomUser.objects.filter(username=username).first()
+            if existing_user is not None and existing_user != request.user:
+                messages.error(request, 'Потребител с това потребителско име вече съществува.')
+                return redirect('edit-profile')
+
+            role_choices = ["user", "hotel_manager", "employee"]
+            role = form.cleaned_data.get('role')
+            if role not in role_choices:
+                messages.error(request, 'Невалидна роля.')
+                return redirect('edit-profile')
+
+
+
+            request.user.first_name = first_name
+            request.user.last_name = last_name
+            request.user.email = email
+            request.user.username = username
+            request.user.role = role
+            request.user.save()
+            messages.success(request, 'Успешно редактиран профил.')
+            return redirect('home')
+
+
+
+    form = EditProfileForm(initial={'first_name': request.user.first_name, 'last_name': request.user.last_name, 'email': request.user.email, 'username': request.user.username, 'role': request.user.role})
+    first_name = request.user.first_name
+    last_name = request.user.last_name
+    email = request.user.email
+    username = request.user.username
+    context = {
+        'form': form,
+        'first_name': first_name,
+        'last_name': last_name,
+        'email': email,
+        'username': username,
+    }
+    return render(request, 'auth/edit_profile.html', context)
+
+
+def change_password(request):
+    if request.method == 'POST':
+        if request.user.has_usable_password():
+            form = ChangePasswordForm(request.POST)
+            if form.is_valid():
+                old_password = form.cleaned_data.get('old_password')
+                new_password1 = form.cleaned_data.get('new_password1')
+                new_password2 = form.cleaned_data.get('new_password2')
+
+                if not request.user.check_password(old_password):
+                    messages.error(request, 'Грешна парола.')
+                    return redirect('change-password')
+
+                if new_password1 != new_password2:
+                    messages.error(request, 'Паролите не съвпадат.')
+                    return redirect('change-password')
+
+                request.user.set_password(new_password1)
+                request.user.save()
+                update_session_auth_hash(request, request.user)
+                messages.success(request, 'Успешно сменена парола.')
+                return redirect('home')
+
+        else:
+            form = SetPasswordFromSocialLogin(request.POST)
+            if form.is_valid():
+                new_password1 = form.cleaned_data.get('new_password1')
+                new_password2 = form.cleaned_data.get('new_password2')
+
+                if new_password1 != new_password2:
+                    messages.error(request, 'Паролите не съвпадат.')
+                    return redirect('change-password')
+
+                request.user.set_password(new_password1)
+                request.user.save()
+                update_session_auth_hash(request, request.user)
+                messages.success(request, 'Успешно сменена парола.')
+                return redirect('home')
+    if request.user.has_usable_password():
+        form = ChangePasswordForm()
+    else:
+        form = SetPasswordFromSocialLogin()
+    return render(request, 'auth/change_password.html', {'form': form})
+
+
+def delete_account(request):
+    if request.method == 'POST':
+        form = DeleteAccountForm(request.POST)
+        if form.is_valid():
+            password = form.cleaned_data.get('password')
+
+            if not request.user.check_password(password):
+                messages.error(request, 'Грешна парола.')
+                return redirect('delete-account')
+
+            request.user.delete()
+            messages.success(request, 'Успешно изтрит потребител.')
+            return redirect('home')
+
+    form = DeleteAccountForm()
+    return render(request, 'auth/delete_account.html', {'form': form})
