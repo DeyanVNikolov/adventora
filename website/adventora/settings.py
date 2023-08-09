a486f27d8a4461d65ee62d260200258ea54656a0
Deyan Nikolov <didonikolovbg@gmail.com>
2023-08-09 22:09:46 +0300

    Migrate "Static" files
    Combine into a single directory

--- website/adventora/settings.py
+++ website/adventora/settings.py
@@ -200,12 +200,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    "static/",
-    os.path.join(BASE_DIR, "static"),
-]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
